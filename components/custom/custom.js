c187fff4c09a7d8052be8d04fbdc46e5d932f67b
wugaofei <wugaofei@58ganji.com>
2019-10-17 20:39:03 +0800

    new

---
+++ components/custom/custom.js
@@ -0,0 +1,16 @@
+// 自定义组件逻辑 (custom.js)
+Component({
+    properties: {
+        // 定义了name属性，可以在使用组件时，由外部传入。此变量可以直接在组件模板中使用
+        name: {
+            type: String,
+            value: 'swan',
+        }
+    },
+    data: {
+        age: 1
+    },
+    methods: {
+        tap: function(){}
+    }
+});
---
+++ pages/index/index.js
@@ -0,0 +1,9 @@
+
+Page({
+    data: {
+        text: '这是一段文字',
+        canAdd: true,
+        canRemove: false,
+        extraLine : [],
+    }
+});
