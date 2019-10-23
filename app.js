2bd1156deedc8f02f5c13b180cd62ed4dfa4815c
wugaofei <wugaofei@58ganji.com>
2019-10-24 01:04:56 +0800

    234

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ App({
     onLaunch(options) {
         // do something when launch
         // 引导添加，参见文档： http://smartprogram.baidu.com/docs/design/component/guide_add/
-        if (swan.canIUse('showFavoriteGuide')) {
-            swan.showFavoriteGuide({
-                type: 'bar',
-                content: '一键关注小程序',
-                success(res) {
-                    console.log('关注成功：', res);
-                },
-                fail(err) {
-                    console.log('关注失败：', err);
-                }
-            });
-        }
+        // if (swan.canIUse('showFavoriteGuide')) {
+        //     swan.showFavoriteGuide({
+        //         type: 'bar',
+        //         content: '一键关注小程序',
+        //         success(res) {
+        //             console.log('关注成功：', res);
+        //         },
+        //         fail(err) {
+        //             console.log('关注失败：', err);
+        //         }
+        //     });
+        // }
     },
     onShow(options) {
         // do something when show
